a4ab4de5c73e15c010f3a883af4efd3f0ca3b97c
Bhaumik Gadani <bhaumikwork@gmail.com>
2016-11-03 17:43:39 +0530

    testing git

---
+++ config/initializers/shopify_app.rb
@@ -0,0 +1,7 @@
+ShopifyApp.configure do |config|
+
+  config.api_key = "f20bf0eb0319ae2fd0f14ab617008f3a"
+  config.secret = "deb0417ce8ce28a364e924d9d5a97157"
+  config.scope = "read_orders, read_products"
+  config.embedded_app = true
+end
